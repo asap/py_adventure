05ba1a2a082fc560be3a0c672472719fac9b6084
ASAP <alexander.sapountzis@gmail.com>
2019-12-07 13:58:47 -0500

    Part 5 - Bumping into things

--- engine.py
+++ engine.py
@@ -1,9 +1,10 @@
 import tcod as libtcod
 
-from entity import Entity
+from entity import Entity, get_blocking_entities_at_location
 from render_functions import clear_all, render_all
 from fov_functions import initialize_fov, recompute_fov
 from map_objects.game_map import GameMap
+from game_states import GameStates
 from input_handlers import handle_keys
 
 
@@ -21,6 +22,8 @@ def main():
     fov_light_walls = True
     fov_radius = 10
 
+    max_monsters_per_room = 3
+
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
         'dark_ground': libtcod.Color(50, 50, 150),
@@ -29,19 +32,11 @@ def main():
     }
 
     player = Entity(
-                 int(screen_width / 2),
-                 int(screen_height / 2),
-                 '@',
-                 libtcod.white
-             )
-    npc = Entity(
-              int(screen_width / 2),
-              int(screen_height / 2),
-              '@',
-              libtcod.yellow
-          )
+        0, 0, '@',
+        libtcod.white,
+        'Player', blocks=True)
 
-    entities = [npc, player]
+    entities = [player]
 
     libtcod.console_set_custom_font(
         'arial10x10.png',
@@ -59,7 +54,8 @@ def main():
     game_map = GameMap(map_width, map_height)
 
     game_map.make_map(max_rooms, room_min_size, room_max_size,
-                      map_width, map_height, player)
+                      map_width, map_height, player, entities,
+                      max_monsters_per_room)
 
     fov_recompute = True
 
@@ -67,6 +63,8 @@ def main():
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
+
+    game_state = GameStates.PLAYERS_TURN
 
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(
@@ -93,17 +91,37 @@ def main():
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')
 
-        if move:
+        if move and game_state == GameStates.PLAYERS_TURN:
             dx, dy = move
-            if not game_map.is_blocked(player.x + dx, player.y + dy):
-                player.move(dx, dy)
-                fov_recompute = True
+            destination_x = player.x + dx
+            destination_y = player.y + dy
+
+            if not game_map.is_blocked(destination_x, destination_y):
+                target = get_blocking_entities_at_location(
+                    entities, destination_x, destination_y)
+
+                if target:
+                    print(f"You bump into the {target.name}. " +
+                          f"You can't understand  what it said, but " +
+                          f"it wasn't pleasant.")
+                else:
+                    player.move(dx, dy)
+                    fov_recompute = True
+
+                game_state = GameStates.ENEMY_TURN
 
         if exit:
             return True
 
         if fullscreen:
             libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+        if game_state == GameStates.ENEMY_TURN:
+            for entity in entities:
+                if entity != player:
+                    print(f"The {entity.name} picks its nose")
+
+            game_state = GameStates.PLAYERS_TURN
 
 
 if __name__ == '__main__':
--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -1,5 +1,7 @@
+import tcod as libtcod
 from random import randint
 
+from entity import Entity
 from map_objects.rectangle import Rect
 from map_objects.tile import Tile
 
@@ -28,6 +30,31 @@ class GameMap:
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
 
+    def place_entities(self, room, entities, max_monsters_per_room):
+        # Get a random number of monsters
+        number_of_monsters = randint(0, max_monsters_per_room)
+
+        for i in range(number_of_monsters):
+            # Choose a random location in the room
+            x = randint(room.x1 + 1, room.x2 - 1)
+            y = randint(room.y1 + 1, room.y2 - 1)
+
+            if not any([entity for entity in entities if entity.x == x and entity.y == y]):
+                if randint(0, 100) < 80:
+                    # 80% chance of an orc
+                    monster = Entity(
+                        x, y, 'o',
+                        libtcod.desaturated_green,
+                        'Orc', blocks=True)
+                else:
+                    # 20% chance of a troll
+                    monster = Entity(
+                        x, y, 'T',
+                        libtcod.darker_green,
+                        'Troll', blocks=True)
+
+                entities.append(monster)
+
     def initialize_tiles(self):
         tiles = [
           [Tile(True) for y in range(self.height)]
@@ -37,7 +64,8 @@ class GameMap:
         return tiles
 
     def make_map(self, max_rooms, room_min_size, room_max_size,
-                 map_width, map_height, player):
+                 map_width, map_height, player, entities,
+                 max_monsters_per_room):
         rooms = []
         num_rooms = 0
 
@@ -96,6 +124,9 @@ class GameMap:
                         self.create_h_tunnel(
                             prev_x, new_x, prev_y)
 
+
+                # place monsters
+                self.place_entities(new_room, entities, max_monsters_per_room)
 
                 # finally, append the new rooms to the list
                 rooms.append(new_room)
